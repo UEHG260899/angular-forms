8b47a46c4addc1e2212df79f3c4f5b0ed9d592c8
Uriel <urielenrique99@gmail.com>
2021-04-10 20:09:45 -0500

    Se agrego funcionalidad a templates dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string,
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number,
+  nombre: string
+}
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -8,9 +18,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+
+  persona: Persona = {
+    nombre: 'Uriel',
+    favoritos: [
+      { id: 1, nombre: 'GTA' },
+      { id: 2, nombre: 'Ark' }
+    ]
+  }
+
+  nuevoJuego!: string;
+
 
   ngOnInit(): void {
+  }
+
+  guardar() {
+    console.log('Fromulario posteado');
+  }
+
+
+  agregar() {
+    this.persona.favoritos.push(
+      {
+        id: this.persona.favoritos.length + 1,
+        nombre: this.nuevoJuego
+      });
+
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
 }
