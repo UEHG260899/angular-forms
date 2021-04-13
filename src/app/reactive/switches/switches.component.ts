89d713479671898f165f644205aebed7dfc7001d
Uriel <urielenrique99@gmail.com>
2021-04-12 21:02:27 -0500

    Se terminaro el componente de reactive switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario : FormGroup = this._fb.group({
+    genero         : [,Validators.required],
+    notificaciones : [, Validators.required],
+    condiciones    : [false, Validators.requiredTrue]
+  });
 
-  ngOnInit(): void {
+  persona = {
+    genero : 'F',
+    notificaciones : true
   }
 
+  constructor(private _fb : FormBuilder) { }
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones : false
+    });    
+
+    //Si se desea cambiar el valor de la persona al momento de cambiar el formulario
+    //this.miFormulario.valueChanges.subscribe( ({condiciones, ...rest}) => {
+      //this.persona = rest;
+    //});
+  }
+
+  guardar(){
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
 }
